3851c8df8b54450188f1e0ab984c2b7b8a0c22e9
NamanSnag <nnag44141@gmail.com>
2023-02-24 00:23:04 +0530

    production build

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const port = 8000;
 require('dotenv').config()
+const port = process.env.PORT || 7000;
 const path = require('path');
 const app = express();
 const db = require('./config/mongoose');
